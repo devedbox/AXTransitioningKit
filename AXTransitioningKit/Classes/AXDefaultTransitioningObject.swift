47962d3a327fdd83f280d227b60c9069fc7ff5f5
devedbox <devedbox@gmail.com>
2017-03-23 11:08:54 +0800

    Added AXDefaultTransitioningObject.

---
+++ AXTransitioningKit/Classes/AXDefaultTransitioningObject.swift
@@ -0,0 +1,13 @@
+//
+//  AXDefaultTransitioningObject.swift
+//  AXTransitioningKit
+//
+//  Created by devedbox on 2017/3/23.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class AXDefaultTransitioningObject: NSObject {
+
+}
