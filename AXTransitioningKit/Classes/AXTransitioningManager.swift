8b3e89b0807afa202cbbc6bbeb10ede1dc65d706
devedbox <devedbox@gmail.com>
2017-03-23 14:36:02 +0800

    Added transitioning manager.

---
+++ AXTransitioningKit/Classes/AXTransitioningManager.swift
@@ -0,0 +1,13 @@
+//
+//  AXTransitioningManager.swift
+//  AXTransitioningKit
+//
+//  Created by devedbox on 2017/3/23.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class AXTransitioningManager: NSObject {
+
+}
